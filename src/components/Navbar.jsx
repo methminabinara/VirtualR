1ba931a6bf63c5421bf725475e570b0ecb248511
Methmina Binara <binaraepa@gmail.com>
2024-06-12 20:17:12 +0530

    feat: Created navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
       <div className="container px-4 mx-auto relative text-sm">
-        <div className="flex justify-center items center">
+        <div className="flex justify-between items center">
           <div className="flex items-center flex-shrink-0">
             <img className="h-10 w-10" src={logo} alt="logo" />
             <span className="text-xl tracking-tight">VirtualR</span>
@@ -17,7 +17,17 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
-          <hidden className="lg-flex justify-center items-center"></hidden>
+          <hidden className="lg-flex justify-center items-center">
+            <a href="#" className="py-2 px-3 border rounded-md">
+              Sign In
+            </a>
+            <a
+              href="#"
+              className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+            >
+              Create an account
+            </a>
+          </hidden>
         </div>
       </div>
     </nav>
