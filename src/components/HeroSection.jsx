44a8534752ad75cc689d366aba9f6c5aba371940
Methmina Binara <binaraepa@gmail.com>
2024-06-16 00:07:53 +0530

    feat: Added header

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video1.mp4";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        VirtualR bulid tool
+        <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
+          {" "}
+          for developers
+        </span>
+      </h1>
+    </div>
+  );
 };
 
 export default HeroSection;
