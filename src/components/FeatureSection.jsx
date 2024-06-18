ba1546b581eaae97307cda51e9573a190eea3b59
Methmina Binara <binaraepa@gmail.com>
2024-06-18 23:00:23 +0530

    feat: Added small title

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-
 const FeatureSection = () => {
-  return <div>FeatureSection</div>;
+  return (
+    <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
+      <div className="text-center">
+        <span className="bg-neutral-900 text-orange-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
+          feature
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default FeatureSection;
