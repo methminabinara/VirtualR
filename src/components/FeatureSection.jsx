8d2939fc987a05be349f5f9de0afd2ff8a2effdc
Methmina Binara <binaraepa@gmail.com>
2024-06-19 12:26:58 +0530

    feat: Added heading text

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -5,6 +5,13 @@ const FeatureSection = () => {
         <span className="bg-neutral-900 text-orange-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
           feature
         </span>
+        <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking wide">
+          Easily build
+          <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+            {" "}
+            your code
+          </span>
+        </h2>
       </div>
     </div>
   );
